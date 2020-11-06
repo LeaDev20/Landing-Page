902a7baf76a19f014fa0f4b4081127c77f62156d
Leandro Nuñez <leandronuniez14@gmail.com>
2020-11-06 14:00:57 -0300

    componente about

--- landing-page/src/components/About/index.js
+++ landing-page/src/components/About/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Grid, Typography, Divider } from '@material-ui/core';
+import testamento from '../../images/testamento.png';
+import mapa from '../../images/mapa.png';
+import idea from '../../images/idea.png';
+import lupa from '../../images/lupa.png';
 import './styles.css';
 
 const About = () => {
@@ -7,6 +11,82 @@ const About = () => {
         <Box className="about">
             <Box p={5}>
                 <Typography variant="h1" align="center">About Us</Typography>
+            </Box>
+            <Box>
+                <Grid container spacing={24}>
+                    <Grid item xs={24} sm={12}>
+                        <Box p={5} m={5} display="flex" flexDirection="column" alignItems="center" justifyContent="center" height={200}>
+                            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                                <Grid container>
+                                    <Grid item xs={12} sm={6}>
+                                        <Box>
+                                            <img src={lupa} className="img-about"/>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item item xs={12} sm={6}>
+                                        <Typography variant="h3">Title</Typography>
+                                        <Typography color="textSecondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                        <Divider variant="inset"/>
+                    </Grid>
+                    <Grid item xs={24} sm={12}>
+                        <Box p={5} m={5} display="flex" flexDirection="column" alignItems="center" justifyContent="center" height={200}>
+                            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                                <Grid container>
+                                    <Grid item xs={12} sm={6}>
+                                        <Box>
+                                            <Typography variant="h3">Title</Typography>
+                                            <Typography color="textSecondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</Typography>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item item xs={12} sm={6}>
+                                        <img src={idea} className="img-about"/>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                        <Divider variant="inset"/>
+                    </Grid>
+                    <Grid item xs={24} sm={12}>
+                        <Box p={5} m={5} display="flex" flexDirection="column" alignItems="center" justifyContent="center" height={200}>
+                            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                                <Grid container>
+                                    <Grid item xs={12} sm={6}>
+                                        <Box>
+                                            <img src={testamento} className="img-about"/>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item item xs={12} sm={6}>
+                                        <Typography variant="h3">Title</Typography>
+                                        <Typography color="textSecondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                        <Divider variant="inset"/>
+                    </Grid>
+                    <Grid item xs={24} sm={12}>
+                        <Box p={5} m={5} display="flex" flexDirection="column" alignItems="center" justifyContent="center" height={200}>
+                            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                                <Grid container>
+                                    <Grid item xs={12} sm={6}>
+                                        <Box>
+                                            <Typography variant="h3">Title</Typography>
+                                            <Typography color="textSecondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</Typography>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item item xs={12} sm={6}>
+                                        <img src={mapa} className="img-about"/>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                        <Divider variant="fullWidth"/>
+                    </Grid>
+                </Grid>
             </Box>
         </Box>
     )
